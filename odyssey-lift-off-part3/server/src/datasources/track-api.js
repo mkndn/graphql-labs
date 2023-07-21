678f79165e4d421effad59d1c9981e25677d42ea
mkdn <iammanirk@gmail.com>
2023-07-21 21:39:48 +0300

    Lift off 3 complete

---
+++ odyssey-lift-off-part3/server/src/datasources/track-api.js
@@ -0,0 +1,24 @@
+const { RESTDataSource } = require("@apollo/datasource-rest");
+
+class TrackAPI extends RESTDataSource {
+  // the Catstronauts catalog is hosted on this server
+  baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
+
+  getTracksForHome() {
+    return this.get("tracks");
+  }
+
+  getAuthor(authorId) {
+    return this.get(`author/${authorId}`);
+  }
+
+  getTrack(id) {
+    return this.get(`track/${id}`);
+  }
+
+  getTrackModules(id) {
+    return this.get(`track/${id}/modules`);
+  }
+}
+
+module.exports = TrackAPI;
